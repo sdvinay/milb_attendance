995178894ea3dca57cc9fdb76cc5a838ca21c73f
Vinay Kumar <sdvinay@gmail.com>
2022-04-15 12:48:43 -0700

    Use typer to enable command-line arguments

--- scrape_attendance.py
+++ scrape_attendance.py
@@ -1,8 +1,9 @@
 import requests
 import pandas as pd
+import typer
 
 
-def get_schedule_url(sport_id, season=2022):
+def get_schedule_url(sport_id:int, season:int = 2022):
     return f'https://statsapi.mlb.com/api/v1/schedule?language=en&sportId={sport_id}&season={season}&sortBy=gameDate&hydrate=gameInfo'
 
 
@@ -12,17 +13,17 @@ fieldnames = ['gamePk', 'gameType', 'officialDate', 'dayNight',
 
 today = str(pd.Timestamp.today().date())
 
-def get_attendance_df(sport_id, season = 2022):
+def get_attendance_df(sport_id:int, season:int = 2022):
     sch = requests.get(get_schedule_url(sport_id, season)).json()
     df = pd.json_normalize(sch['dates'], record_path=['games'])[fieldnames].set_index('gamePk').query('officialDate < @today')
     df['dayOfWeek'] = pd.to_datetime(df['officialDate']).dt.day_name()
     return df
 
-def main(season = 2022):
+def main(season:int = 2022):
     # Each MILB level has its own sport_id, so iterate over them
     att = pd.concat([get_attendance_df(sport_id, season) for sport_id in [11, 12, 13, 14]])
     att.sort_values('officialDate', ascending=False).to_csv(f'attendance_{season}.txt')
     
 
 if __name__ == "__main__":
-    main()
+    typer.run(main) # typer will enable user to set the season from the command line with --season
