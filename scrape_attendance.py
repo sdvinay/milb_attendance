ebb200d05c03a733fffd2a3484aa8de902f50411
Vinay Kumar <sdvinay@gmail.com>
2022-04-15 18:13:05 -0700

    separate logic of getting data from filtering/formatting for output

--- scrape_attendance.py
+++ scrape_attendance.py
@@ -7,7 +7,7 @@ def get_schedule_url(sport_id:int, season:int = 2022):
     return f'https://statsapi.mlb.com/api/v1/schedule?language=en&sportId={sport_id}&season={season}&sortBy=gameDate&hydrate=gameInfo'
 
 
-fieldnames = ['gamePk', 'gameType', 'officialDate', 'dayNight',
+fieldnames = ['gameType', 'officialDate', 'dayNight',
  'teams.away.team.id', 'teams.away.team.name', 'teams.home.team.id',
  'teams.home.team.name', 'gameInfo.attendance']
 
@@ -15,14 +15,18 @@ today = str(pd.Timestamp.today().date())
 
 def get_attendance_df(sport_id:int, season:int = 2022):
     sch = requests.get(get_schedule_url(sport_id, season)).json()
-    df = pd.json_normalize(sch['dates'], record_path=['games'])[fieldnames].set_index('gamePk').query('officialDate < @today')
-    df['dayOfWeek'] = pd.to_datetime(df['officialDate']).dt.day_name()
+    df = pd.json_normalize(sch['dates'], record_path=['games']).set_index('gamePk')
     return df
+
+def write_output(df: pd.DataFrame, output_file: str):
+    df_out = df[fieldnames].query('officialDate < @today').sort_values('officialDate', ascending=False)
+    df_out['dayOfWeek'] = pd.to_datetime(df_out['officialDate']).dt.day_name()
+    df_out.to_csv(output_file)
 
 def main(season:int = 2022):
     # Each MILB level has its own sport_id, so iterate over them
     att = pd.concat([get_attendance_df(sport_id, season) for sport_id in [11, 12, 13, 14]])
-    att.sort_values('officialDate', ascending=False).to_csv(f'attendance_{season}.txt')
+    write_output(att, f'attendance_{season}.txt')    
     
 
 if __name__ == "__main__":
