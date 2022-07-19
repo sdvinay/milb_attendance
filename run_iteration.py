f74542825629a31270a4d250df3d8aa367197b08
Vinay Kumar <sdvinay@gmail.com>
2022-07-18 18:20:07 -0700

    more scaffolding

--- run_iteration.py
+++ run_iteration.py
@@ -1,6 +1,7 @@
 import os
 import time
 import logging
+import typer
 import scrape_attendance as sa
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
@@ -10,21 +11,32 @@ REMOTE_USER='u35698910'
 REMOTE_HOST='home92388257.1and1-data.host'
 REMOTE_DIR='kumars/vinay/milb_attendance_data'
 
-NUM_LOOPS=1000
+NUM_LOOPS_DEFAULT=1000
 SLEEP_INTERVAL=10
 
-for i in range(NUM_LOOPS):
+def run_shell_command(cmd: str):
+    logging.info(f'Running shell command: {cmd}')
+    output = os.popen(cmd).read()
+    logging.info(output)
+    return output
+
+def run_iteration(i: int, num_loops: int):
     logging.info(f'Starting iteration {i}')
     sa.main()
-    local_dir_listing = os.popen('ls -ltr output').read()  # see the local output
-    logging.info(local_dir_listing)
-    rsync_output = os.popen(f'rsync -ahzi output/ {REMOTE_USER}@{REMOTE_HOST}:{REMOTE_DIR}').read() # sync
-    logging.info(rsync_output)
+    run_shell_command('ls -ltr output') # see the local output
+    run_shell_command(f'rsync -ahzi output/ {REMOTE_USER}@{REMOTE_HOST}:{REMOTE_DIR}')  # sync
     # ssh ${REMOTE_USER}@${REMOTE_HOST} ls -ltr ${REMOTE_DIR} # see the remote output
-    # echo `date` Finished iteration $i
+    logging.info(f'Finished iteration {i}')
 
     # # sleep (unless we're on the last iteration)
-    if i < NUM_LOOPS:
-        os.system(f'echo sleeping {SLEEP_INTERVAL} seconds')
+    if i < num_loops:
+        logging.info(f'sleeping {SLEEP_INTERVAL} seconds')
         time.sleep(SLEEP_INTERVAL)
 
+def main(num_loops: int = NUM_LOOPS_DEFAULT):
+    for i in range(num_loops):
+        run_iteration(i, num_loops)
+
+
+if __name__ == "__main__":
+    typer.run(main) # typer will enable user to set the season from the command line with --season
