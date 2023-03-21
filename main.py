4b9c511f0f1e1821ce4fed8a0019cc9d6a372223
Vinay Kumar <sdvinay@gmail.com>
2023-03-21 15:08:24 -0700

    Add summary report to HTTP API

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ import scrape_attendance as sa
 app = FastAPI()
 
 @app.get("/team_map", response_class=HTMLResponse)
-# Merge the teams with league data to get additional information (level, sortOrder)
 def generate_team_map(season: int = 2022) -> HTMLResponse:
     df = sa.generate_team_map(season)
+    html = df.to_html()
+    return HTMLResponse(content=html)
+
+@app.get("/summary_report", response_class=HTMLResponse)
+def generate_summary_report(season: int = 2022) -> HTMLResponse:
+    att = sa.get_attendance_all_levels(season)
+    df = sa.generate_summary_report(att)
     html = df.to_html()
     return HTMLResponse(content=html)
